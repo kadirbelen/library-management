d832b7e64e98f201ed9cf7618702370e57412d51
kadir belen <kadir@bordo.io>
2024-11-28 21:36:23 +0300

    style: rename body validator middleware

--- src/routers/book.router.ts
+++ src/routers/book.router.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
 import bookController from '../controllers/book.controller';
+import bodyValidator from '../middlewares/body-validator.middleware';
 import { paramsIdValidator } from '../middlewares/params-id-validator.middleware';
-import validator from '../middlewares/validation.middleware';
 import { createBookSchema } from '../validations/book.validation';
 
 const router = express.Router();
 
-router.post('/', validator(createBookSchema), bookController.create);
+router.post('/', bodyValidator(createBookSchema), bookController.create);
 router.get('/:bookId', paramsIdValidator, bookController.get);
 router.get('/', bookController.list);
 
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,16 +2,16 @@ import express from 'express';
 
 import borrowController from '../controllers/borrow.controller';
 import userController from '../controllers/user.controller';
+import bodyValidator from '../middlewares/body-validator.middleware';
 import { paramsIdValidator } from '../middlewares/params-id-validator.middleware';
-import validator from '../middlewares/validation.middleware';
 import { returnBookSchema } from '../validations/borrow.validation';
 import { createUserSchema } from '../validations/user.validation';
 
 const router = express.Router();
 
-router.post('/', validator(createUserSchema), userController.create);
+router.post('/', bodyValidator(createUserSchema), userController.create);
 router.post('/:userId/borrow/:bookId', paramsIdValidator, borrowController.borrowBook);
-router.post('/:userId/return/:bookId', paramsIdValidator, validator(returnBookSchema), borrowController.returnBook);
+router.post('/:userId/return/:bookId', paramsIdValidator, bodyValidator(returnBookSchema), borrowController.returnBook);
 router.get('/:userId', paramsIdValidator, userController.get);
 router.get('/', userController.list);
 
